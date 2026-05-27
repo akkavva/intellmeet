bc2f4197619415521ae993f448950f71418843d4
akkamma <akkammadange8431@gmail.com>
2026-05-27 14:25:35 +0530

    feat: add debug-sentry route

--- server/index.js
+++ server/index.js
@@ -70,6 +70,10 @@ app.get('/', (req, res) => {
   res.json({ message: 'IntellMeet API is running!' });
 });
 
+app.get('/debug-sentry', (req, res) => {
+  throw new Error('Sentry test error from IntellMeet!');
+});
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
